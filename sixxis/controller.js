ad46e7bbb8771daf3a8ce88944305b4eb539acc0
zhufengzhufeng <1035465284@qq.com>
2016-09-25 18:48:43 +0800

    ok

--- sixxis/controller.js
+++ sixxis/controller.js
@@ -8,7 +8,11 @@ angular.module('ctrl',[])
         $timeout(function () {
             $scope.users =
         },1000)*/
-    }).controller('listCtrl',function ($scope,$timeout,$stateParams) {
-         $scope.id =$stateParams.id; //获取路由中的参数
+    }).controller('listCtrl',function ($scope,$timeout,$stateParams,$state) {
+        $scope.id =$stateParams.id; //获取路由中的参数
+        $scope.goIndex = function () { //$location
+            //$state这个服务用来实现页面跳转的
+            $state.go('list',{id:2}); //js跳转并传入参数
+        }
     });
 //创建一个独立的控制器模块，方便我们书写控制器
